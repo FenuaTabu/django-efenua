ec7c45fe50f3bbe48c86253798ce7b488ab6d92c
peter <peter@192.168.1.17>
2016-03-05 16:45:46 +0100

    bug mineur

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, patterns
+from efenua import views
 
 urlpatterns = [
-    url(r'^admin/yuml/$', 'efenua.views.yuml', name='yuml'),
+    url(r'^admin/yuml/$', views.yuml, name='yuml'),
 ]
