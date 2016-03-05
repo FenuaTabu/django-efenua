4433ef802547f38c3e6656316cc06339c32ddb2a
peter <peter@192.168.1.17>
2016-03-05 16:10:43 +0100

    Amelioration du raw_id_fields

--- admin/models.py
+++ admin/models.py
@@ -9,6 +9,50 @@ from django.http import Http404, HttpResponseRedirect
 from django.http.response import HttpResponseBase
 from django.core.urlresolvers import reverse
 
+from django.contrib.admin.widgets import ManyToManyRawIdWidget, ForeignKeyRawIdWidget
+from django.contrib.admin.sites import site
+
+from django import forms
+from django.utils.html import escape
+
+class VerboseForeignKeyRawIdWidget(ForeignKeyRawIdWidget):
+    def label_for_value(self, value):
+        key = self.rel.get_related_field().name
+        try:
+            obj = self.rel.to._default_manager.using(self.db).get(**{key: value})
+            
+            if 'raw_id_value' in dir(obj):
+                    str_values += [obj.raw_id_value()]
+            else:
+                change_url = reverse(
+                    "admin:%s_%s_change" % (obj._meta.app_label, obj._meta.object_name.lower()),
+                    args=(obj.pk,)
+                )
+                return '&nbsp;<strong><a href="%s">%s</a></strong>' % (change_url, escape(obj))
+        except (ValueError, self.rel.to.DoesNotExist):
+            return '???'
+
+class VerboseManyToManyRawIdWidget(ManyToManyRawIdWidget):
+    def label_for_value(self, value):
+        values = value.split(',')
+        str_values = []
+        key = self.rel.get_related_field().name
+        for v in values:
+            try:
+                obj = self.rel.to._default_manager.using(self.db).get(**{key: v})
+                if 'raw_id_value' in dir(obj):
+                    str_values += [obj.raw_id_value()]
+                else:
+                    x = obj
+                    change_url = reverse(
+                        "admin:%s_%s_change" % (obj._meta.app_label, obj._meta.object_name.lower()),
+                        args=(obj.pk,)
+                    )
+                    str_values += ['<strong><a href="%s">%s</a></strong>' % (change_url, escape(x))]
+            except self.rel.to.DoesNotExist:
+                str_values += [u'???']
+        return u', '.join(str_values)
+
 class EfenuaModelAdmin(admin.ModelAdmin):
     
     """
@@ -27,6 +71,17 @@ class EfenuaModelAdmin(admin.ModelAdmin):
     """
     objectactions = []
     tools_view_name = None
+    
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name in self.raw_id_fields:
+            kwargs.pop("request", None)
+            type = db_field.rel.__class__.__name__
+            if type == "ManyToOneRel":
+                kwargs['widget'] = VerboseForeignKeyRawIdWidget(db_field.rel, site)
+            elif type == "ManyToManyRel":
+                kwargs['widget'] = VerboseManyToManyRawIdWidget(db_field.rel, site)
+            return db_field.formfield(**kwargs)
+        return super(EfenuaModelAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
     def get_tool_urls(self):
         """Get the url patterns that route each tool to a special view."""
